dcee51063ee818efcc47251d03f715a40ac2c5e1
Philo <philoatefdev@gmail.com>
2024-07-22 22:50:58 +0300

    final proj

--- Assignment10/Program.cs
+++ Assignment10/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Assignment10
+﻿using Project2;
+
+namespace Assignment10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Maths.Add(1, 2));
+            Console.WriteLine(Maths.Subtract(10, 4));
+            Console.WriteLine(Maths.Divide(10, 2));
+            Console.WriteLine(Maths.Multiply(2, 2));
         }
     }
 }
---
+++ FirstProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using Project1;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Point3D P = new Point3D(10, 10, 10);
+            Console.WriteLine(P.ToString());
+
+            Point3D[] points = new Point3D[2];
+            for (int i = 1; i < 3; i++)
+            {
+                bool isAllParsed = true;
+
+                Console.WriteLine($"enter X for point {i}: ");
+                isAllParsed &= int.TryParse(Console.ReadLine(), out int x);
+                Console.WriteLine($"enter Y for point {i}: ");
+                isAllParsed &= int.TryParse(Console.ReadLine(), out int y);
+                Console.WriteLine($"enter Z for point {i}: ");
+                isAllParsed &= int.TryParse(Console.ReadLine(), out int z);
+
+                if (isAllParsed)
+                    points[i - 1] = new Point3D(x, y, z);
+                else
+                    i--;
+            }
+
+            //no, because the base implementation compares references
+            Console.WriteLine($"point 1 == point 2? : {points[0] == points[1]}");
+
+            Point3D[] pointsarr =
+            {
+                new Point3D (10, 20, 10),
+                new Point3D (10, 10, 10),
+                new Point3D (30, 30, 30),
+            };
+
+            Array.Sort(pointsarr);
+            foreach (Point3D point in pointsarr)
+            {
+                Console.WriteLine(point);
+            }
+
+            Point3D p1 = new Point3D(10, 20, 30);
+            Point3D p2 = (Point3D)p1.Clone();
+            
+        }
+    }
+}
